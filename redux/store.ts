f5c35c57e4415d9cee8cfa4c07186e580db2dce1
Nibir Hossain <Nibir.Hossain85@gmail.com>
2025-10-08 18:12:36 +0200

    Story 5.2 (#30): integrated @redux-devtools/extension plugin

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,10 @@
 import { createStore, combineReducers } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { chatInterfaceReducer } from "./chat-interface-reducer";
 
 const rootReducer = combineReducers({
   chatInterface: chatInterfaceReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 export default store;
