0b4e4034667ee40c6cebd097131967416b9db67b
Nibir Hossain <Nibir.Hossain85@gmail.com>
2025-10-26 10:11:03 +0100

    Epic-7 (#41): Integrated tanstack react-query and react-query-devtools

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,8 @@ import type React from "react";
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import { Analytics } from "@vercel/analytics/next";
-import { LanguageProvider } from "@/contexts/language-context";
-import { ThemeProvider } from "@/contexts/theme-context";
-import { Suspense } from "react";
+import { Providers } from "./providers";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,18 +14,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-        <Suspense fallback={null}>
-          <ThemeProvider>
-            <LanguageProvider>{children}</LanguageProvider>
-          </ThemeProvider>
-        </Suspense>
-        <Analytics />
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
